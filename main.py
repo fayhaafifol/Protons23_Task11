80c3107fa496dbf26f3eea3c9c2e01ed4639ae3e
fayhaafifol <fayhaaalsakkar@gmail.com>
2023-08-22 00:48:15 +0300

    fayhaa was here

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-#TODO Calculator FUNCTIONS
 
 
+def calculate(num1, num2,op):
 
+        global result
 
+        result=0
 
+        if op =="1":
+             
+                result= num1 + num2
 
-def calculate(a, b, operation):
-        #TODO (CONTINUE ON THE SAME PATTERN
+        elif op =="3":
+               
+                result= num1/num2
+        
 
+op = input("Choose the opertion that you desire (^u^)/ :\n 1- Addition\n 2- Subtraction\n 3- Division\n 4- Multiplication\n")
 
+num1 = float(input("Please enter your first number (OwO):"))
 
+num2 = float(input("Please enter your second number ('u'):"))
 
-num1 = #TODO INPUT FROM USER
-num2 = #TODO INPUT FROM USER
-op = #TODO INPUT FROM USER
-print(calculate(num1, num2, op))
+calculate(num1, num2,op)
+
+print("This is your result \(>w<)/:",result)
+
